8cfd6e1bf04866ec0f1fbb39eaeb1bcfba5be506
Jose Ignacio Palma <ignacio.palma@edunext.co>
2021-04-29 16:08:39 -0400

    feat: add extra_script and add script to context_processors

--- ecommerce_extensions/tenant/context_processors.py
+++ ecommerce_extensions/tenant/context_processors.py
@@ -4,6 +4,7 @@ This file contains django context_processors needed by edunext.
 """
 import logging
 
+from ecommerce_extensions.tenant.extra_scripts import process_scripts
 from ecommerce_extensions.tenant.models import TenantOptions
 
 LOG = logging.getLogger(__name__)
@@ -25,8 +26,11 @@ def theme_options(request):
     except AttributeError:
         site_theme_name = None
 
+    theme_options_values = options.get("THEME_OPTIONS", {})
+
     return {
         "theme_dir_name": site_theme_name,
         "site_configuration": request.site.siteconfiguration,
         "options": options,
+        "scripts": process_scripts(request.path_info, theme_options_values)
     }
